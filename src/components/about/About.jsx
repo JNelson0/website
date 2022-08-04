f01d086bf8c5b8b4febb86d12c178e22ba75895a
Jared Nelson <jaredlnelson1@gmail.com>
2022-08-03 23:42:50 -0400

    About and Intro page edits
    Need to figure out information for about page.  Intro page should be finished

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -2,52 +2,48 @@ import "./about.scss";
 import Terminal from "../terminal/Terminal";
 import { React, useState } from "react";
 
+import { Button, Row, Col, Container } from "react-bootstrap";
+import { Calculate } from "@mui/icons-material";
+
 export default function About() {
-    const [terminalActive, setTerminalActive] = useState(false);
-    return (
-        <div className="about" id="about">
-            {terminalActive ? (
-                <div className="terminal">
-                    <Terminal setTerminalActive={setTerminalActive} />
-                </div>
-            ) : (
-                <div className="about">
-                    <div className="topWrapper">
-                        <h1>About Me</h1>
-                        <span className="line"></span>
-                        <span className="line2"></span>
-                    </div>
-                    <div className="bottomWrapper">
-                        <p>
-                            I am currently in my final semester at the
-                            University of Nevada Reno studying Computer Science
-                            and Engineering.
-                        </p>
-                        <p>
-                            I have a strong passion for software development,
-                            machine learning, full stack web development, and
-                            application development. I am constantly learning
-                            and working on projects to strengthen my coding
-                            potential.
-                        </p>
-                        <h2>Learn more about my:</h2>
-                        <div className="linkClicker">
-                            <a className="portfolio" href="#portfolio">
-                                Projects
-                            </a>
-                            <a className="skills" href="#skills">
-                                Skills
-                            </a>
-                        </div>
-                    </div>
-                    <div
-                        className="goBack"
-                        onClick={() => setTerminalActive(true)}
-                    >
-                        Check this out!!!
-                    </div>
-                </div>
-            )}
-        </div>
-    );
+	const [terminalActive, setTerminalActive] = useState(false);
+	return (
+		<Container className="outer" id="about" fluid>
+			<Col className="c1" md>
+				Need something to fill the void
+			</Col>
+			<Col className="c2" md>
+				<h2>About me</h2>
+
+				<p>
+					I am a recent graduate from the University of Nevada Reno,
+					where I recieved a Bachelor's degree in Computer Science and
+					Engineering.
+				</p>
+				<p>
+					I have a strong passion for software development, machine
+					learning, full stack web development, and application
+					development. I am constantly learning and working on
+					projects to strengthen my coding potential.
+				</p>
+				<p>
+					Outside of coding I enjoy traveling, backpacking, and
+					anything outdoors.
+				</p>
+				<h2>Learn more about my:</h2>
+				<div className="buttons">
+					<Button variant="outline-secondary" href="#portfolio">
+						Portfolio
+					</Button>
+					<Button
+						className="m-3"
+						variant="outline-secondary"
+						href="#skills"
+					>
+						Skills
+					</Button>
+				</div>
+			</Col>
+		</Container>
+	);
 }
--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -17,29 +17,22 @@ export default function Intro() {
 		});
 	}, []);
 	return (
-		<Container
-			className="container"
-			fluid
-			style={{ width: "70%" }}
-			id="intro"
-		>
-			<Row>
-				<Col className="left" md>
-					<div className="image mt-3">
-						<Image src={me} responsive />
-					</div>
-				</Col>
-				<Col className="right" md>
-					<h2>Hi There, I'm</h2>
-					<h1>Jared Nelson</h1>
-					<h3>
-						Software <span ref={textRef}></span>
-					</h3>
-					<a href="#about">
-						<img src={arrow} alt="" />
-					</a>
-				</Col>
-			</Row>
+		<Container className="container" fluid id="intro">
+			<Col className="left">
+				<div className="image mt-3">
+					<Image src={me} responsive />
+				</div>
+			</Col>
+			<Col className="right">
+				<h2>Hi There, I'm</h2>
+				<h1>Jared Nelson</h1>
+				<h3>
+					Software <span ref={textRef}></span>
+				</h3>
+				<a href="#about">
+					<img src={arrow} alt="" />
+				</a>
+			</Col>
 
 			{/* <div className="intro" id="intro">
 				<div className="left">
